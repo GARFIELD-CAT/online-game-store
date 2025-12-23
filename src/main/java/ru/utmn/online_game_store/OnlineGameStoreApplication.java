38ff885cd210f9129fc02ebd417cb7a01547c566
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-12-23 21:53:50 +0300

    init project commit

---
+++ src/main/java/ru/utmn/online_game_store/OnlineGameStoreApplication.java
@@ -0,0 +1,14 @@
+package ru.utmn.online_game_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineGameStoreApplication.class, args);
+        System.out.println("Ok");
+	}
+
+}
