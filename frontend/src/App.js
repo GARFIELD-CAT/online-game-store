cfea56d3ffa1b329f4c6bee5ec938729d9c0de56
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-12-29 12:44:01 +0300

    fix init frontend logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,55 @@ import OrderPage from "./pages/OrderPage";
 import LoginPage from "./pages/LoginPage";
 import { routes } from "./utils/routes";
 import OrderListPage from "./pages/OrderListPage";
+import { AuthProvider, useAuth } from "./utils/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+
+  console.log(isAuthenticated);
+
+  return isAuthenticated ? (
+    children
+  ) : (
+    <Navigate to={routes.LOGIN_PAGE} replace />
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route exact path={routes.LOGIN_PAGE} element={<LoginPage />} />
-        <Route exact path={routes.MAIN_PAGE} element={<MainPage />} />
-        <Route path={routes.ORDER_PAGE} element={<OrderPage />} />
-        <Route path={routes.ORDER_LIST_PAGE} element={<OrderListPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </div>
+    <AuthProvider value={{ isAuthenticated: true }}>
+      <div className="App">
+        <Routes>
+          <Route exact path={routes.LOGIN_PAGE} element={<LoginPage />} />
+          <Route
+            exact
+            path={routes.MAIN_PAGE}
+            element={
+              <ProtectedRoute>
+                <MainPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path={routes.ORDER_PAGE}
+            element={
+              <ProtectedRoute>
+                <OrderPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path={routes.ORDER_LIST_PAGE}
+            element={
+              <ProtectedRoute>
+                <OrderListPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
